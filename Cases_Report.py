88d9c6bb0176bd368adb20b2c4f1aec10c37ee96
yoyo890929 <yoyo890929@gmail.com>
2022-06-12 12:07:43 +0800

    ver 1.1 Add some input check

--- Cases_Report.py
+++ Cases_Report.py
@@ -3,78 +3,13 @@ from pyignite import Client
 import random
 client = Client()
 client.connect('127.0.0.1', 10800)
-"""
-#*註冊*********************************************#
-name = input("註冊姓名 : ")
-account = input("註冊帳號(手機號碼) : ")
-password = input("註冊密碼 : ")
 
-ACCOUNT_SELECT_QUERY = "SELECT * FROM Account WHERE ID ='" + account + "' AND password = " + "'"+password +"'"
-accounts = client.sql(ACCOUNT_SELECT_QUERY)
-
-count = 0 #如果accounts篩出來是空的 count就會是0 檢測是否有註冊
-for row in accounts:
-    count=count+1
-if count == 0 :
-    print("可以註冊")
-    ACCOUNT_INSERT_QUERY = '''INSERT INTO Account(
-        Name, ID, Password, Date , Place
-    ) VALUES (?, ?, ?, ?,?)'''
-
-    ACCOUNT_DATA = [
-        [name,account,password, '#','#']
-    ]
-
-    for row in ACCOUNT_DATA:
-        client.sql(ACCOUNT_INSERT_QUERY, query_args=row)
-else :
-    print("已經註冊過了")
-#*************************************************#
-
-#**顯示接觸確診者記錄******************************#
-yesOrNo = input("是否要查詢有無接觸確診者(Y/N)")
-
-if yesOrNo == 'Y' :
-    ##將帳號密碼對應帳號的 Name 日期 地點 匯入
-    FOOTPRINT_SELECT_QUERY = "SELECT Name,Date,Place FROM Footprint WHERE ID = '" + account +"'"
-    footprints = client.sql(FOOTPRINT_SELECT_QUERY)
-    ##將確診者的資料匯入
-    CONFIRMED_SELECT_QUERY = "SELECT Name,Date,Place FROM Confirmed"
-    confirmeds = client.sql(CONFIRMED_SELECT_QUERY)
-
-    count = 0
-    c = 0 
-    ##遍歷所有資料
-    for row in footprints :
-        place = row[2]
-        date = row[1]
-        ##將確診者與足跡中地點時間相同者的資料匯入
-        #(變數) = SELECT 目標欄位 WHERE 條件相符
-        CONFIRMED_SELECT_QUERY = "SELECT Name,Date,Place FROM Confirmed WHERE Place = '"+place+"' AND Date = '"+date +"'"
-        #一定要加client.sql才會執行 要不然只是字串
-        confirmeds = client.sql(CONFIRMED_SELECT_QUERY)
-        count = 0
-        for r in confirmeds:
-            count = count+1
-        if count != 0 :
-            print("有接觸")
-            print(*r)
-            c = c+1
-    if c == 0 :
-        print("無接觸")
-#*****************************************************#
-"""
 
 #****新增確診者****************************************#
 name = input("通報確診者姓名 : ")
 account = input("通報確診者帳號(手機號碼) : ")
 date = input("通報時間: ")
 
-"""
-#找出確診者足跡
-FOOTPRINT_SELECT_QUERY = "SELECT * FROM Footprint WHERE ID = '" + account +"'"
-footprints = client.sql(FOOTPRINT_SELECT_QUERY)
-"""
 r = random.randint(0, 100000000)
 CONFIRMED_INSERT_QUERY = "INSERT INTO Confirmed(Name, ID, Date, key) VALUES ("
 CONFIRMED_INSERT_QUERY += "\'" + name + "\', "
@@ -83,13 +18,9 @@ CONFIRMED_INSERT_QUERY += "\'" + date + "\', "
 CONFIRMED_INSERT_QUERY += "\'" + str(r) + "\')"
 
 client.sql(CONFIRMED_INSERT_QUERY)
-"""
-for row in footprints :
-    INSERT_DATA = [[row[0],row[1],row[2],row[3],row[4],random.randint(0, 100000000)]]
-    for rows in INSERT_DATA:
-        client.sql(CONFIRMED_INSERT_QUERY, query_args=rows)
 
-#***印出所有確診足跡****************#
+"""
+#***印出所有確診與足跡****************#
 CONFIRMED_SELECT_QUERY = "SELECT * FROM Confirmed"
 confirmeds = client.sql(CONFIRMED_SELECT_QUERY)
 for confirmed in confirmeds:
--- ignite_member/app.py
+++ ignite_member/app.py
@@ -31,27 +31,27 @@ def homepage():
 def signin():
 
     #前端接收資料
-    phone = request.form["phone"]
-    password= request.form["password"]
+    phone = request.form.get("phone")
+    password= request.form.get("password")
+
+    if( len(str(phone))!=10 ) :
+        return render_template("homepage.html",msg="請輸入正確的手機號碼")
 
     #根據資料與資料庫互動
-    # ACCOUNT_SELECT_QUERY = "SELECT * FROM Account WHERE ID ='" + phone + "' AND password = " + "'"+password +"'"
     ACCOUNT_SELECT_QUERY = "SELECT * FROM Account WHERE ID ='" + phone +"'"
     accounts = client.sql(ACCOUNT_SELECT_QUERY)
-    
-    
+     
     # 判斷電話號碼資料庫裡有沒有
-    # 有 -> 密碼錯誤 -> 再輸入一次
-    # 有 -> 密碼正確 -> 導入會員頁面(memberpage) -> 印出接觸史
-    #沒有-> 註冊頁面註冊(signin) -> 成功 -> 導回登入頁面(homepage)
+    # 有 -> 密碼錯誤 -> 錯誤訊息
+    # 有 -> 密碼正確 -> 導入會員頁面(memberpage)
+    #沒有-> 錯誤訊息 -> 導回登入頁面(homepage)
     #(問題：為什麼輸入太短會error)
 
-    ###############       有問題       ###################
     count = 0
     for row in accounts:
         count=count+1
     if count == 0 :
-        return render_template("homepage.html",msg="帳號或密碼輸入錯誤")  
+        return render_template("homepage.html",msg="手機號碼或密碼輸入錯誤")  
     else :
         ACCOUNT_SELECT_QUERY = "SELECT * FROM Account WHERE ID ='" + phone + "' AND password = " + "'"+password +"'"
         ac = client.sql(ACCOUNT_SELECT_QUERY)
@@ -59,7 +59,7 @@ def signin():
         for row in ac:
             cnt=cnt+1
         if cnt == 0 :
-            return render_template("homepage.html",msg="帳號或密碼輸入錯誤")  
+            return render_template("homepage.html",msg="手機號碼或密碼輸入錯誤")  
         else:
             session["phone"] = phone
             return redirect("\member") 
@@ -78,27 +78,19 @@ def signuppage():
     password = request.form["password"]
     password_2 = request.form["password_2"]
     
-
     #註冊
     #輸入電話及密碼、確認密碼(不得與其他手機號碼重複)
-    #確認後將資料輸入到SQL（怎麼插入）
+    #確認後將資料輸入到SQL
     #導回首頁(homepage)，印出註冊成功
-
-    ###############       怎麼插       ###################
     
-    #ACCOUNT_SELECT_QUERY = "SELECT * FROM Account WHERE ID ='" + phone +"'"
     ACCOUNT_SELECT_QUERY = "SELECT Name, ID FROM Account"
     accounts = client.sql(ACCOUNT_SELECT_QUERY)
 
+    #如果 accounts 中的 name(row[0]) 有重複，回傳已註冊訊息
     for row in accounts:
         if( row[0] == name):
-            return render_template("homepage.html",msg="此帳號已被註冊!!")
-    #count = 0 #如果accounts篩出來是空的 count就會是0 檢測是否有註冊
-
-    #for row in accounts:
-    #    count=count+1
-
-    #if count == 0 :
+            return render_template("homepage.html",msg="此手機已被註冊!!")
+    
     if password != password_2:
         return render_template("signuppage.html",msg="兩次密碼不符")
     ACCOUNT_INSERT_QUERY = "INSERT INTO Account(Name, ID, Password) VALUES ("
@@ -107,13 +99,6 @@ def signuppage():
     ACCOUNT_INSERT_QUERY += "\'" + password + "\')"
 
     client.sql(ACCOUNT_INSERT_QUERY)
-
-        #ACCOUNT_DATA = [
-        #    [name,phone,password, '#','#']
-        #]
-
-        #for row in ACCOUNT_DATA:
-            #client.sql(ACCOUNT_INSERT_QUERY, query_args=row)
     return render_template("homepage.html",msg="註冊成功")
     #else :
     #    return render_template("homepage.html",msg="手機已被註冊")
@@ -130,25 +115,18 @@ def signout():
 
 @app.route("/member") 
 def memberpage():
-    #有接觸史 -> 印出接觸的日期、時間、地點等等
-    #無接觸史 
-    #要有一個登出按鈕 -> 導回首頁
-
+    #返回會員頁面
     if "phone" in session:
         return render_template("memberpage.html") 
     else:
         return redirect("/")
 
-## 主要寫這 ##
-## 搜尋接觸史 ##
 
+## 搜尋接觸史 ##
 @app.route("/connectsearch",methods=["POST"])
 def connectsearch():
-    ##將帳號密碼對應帳號的 phone 日期 地點 匯入
     if "phone" in session:
         phone = session["phone"]
-        #FOOTPRINT_SELECT_QUERY = "SELECT Name, Date, Place FROM Footprint WHERE ID = '" + phone +"'"
-        #footprints = client.sql(FOOTPRINT_SELECT_QUERY)
         ##將確診者的資料匯入
         CONFIRMED_SELECT_QUERY = "SELECT Name, ID FROM Confirmed"
         confirmeds = client.sql(CONFIRMED_SELECT_QUERY)
@@ -176,12 +154,10 @@ def connectsearch():
                     print("有接觸")
                     s = ''.join(ch for ch in row_s[0] if ch.isdigit())
                     s_o = ''.join(ch for ch in msg2 if ch.isdigit())
-                    #print(msg2)
                     if( s >= s_o ) :
                         msg2 = row_s[0]
                         msg3 = row_s[1]
-                    c = c+1                   
-        #print(c)      
+                    c = c+1                      
         if( c!=0 ) :
             return render_template("connectpage.html",msg=phone,msg2=msg2,msg3=msg3)
         msg2 = "無"
@@ -189,40 +165,10 @@ def connectsearch():
         return render_template("connectpage.html",msg=phone,msg2=msg2,msg3=msg3) 
 
     else:
-        return redirect("/") 
-
-        """
-        count = 0
-        c = 0 
-        ##遍歷所有資料
-        for row in footprints :
-            place = row[2]
-            date = row[1]
-            ##將確診者與足跡中地點時間相同者的資料匯入
-            #(變數) = SELECT 目標欄位 WHERE 條件相符
-            CONFIRMED_SELECT_QUERY = "SELECT Name, Date, Place FROM Confirmed WHERE Place = '"+place+"' AND Date = '"+date +"'"
-            #一定要加client.sql才會執行 要不然只是字串
-            confirmeds = client.sql(CONFIRMED_SELECT_QUERY)
-            count = 0
-            for r in confirmeds:
-                count = count+1
-            if count != 0 :
-                print("有接觸")
-                msg2=r[1]
-                msg3=r[2]
-                c = c+1
-                return render_template("connectpage.html",msg=phone,msg2=msg2,msg3=msg3)  
-        if c == 0 :
-            msg2 = "無"
-            msg3 = "無"
-        return render_template("connectpage.html",msg=phone,msg2=msg2,msg3=msg3) 
-
-        """
-    
+        return redirect("/")   
 
 
-## 主要寫這 ##
-## 確診回報系統、輸入驗證碼、填入資料 ##
+## 足跡回報系統 ##
 @app.route("/diagnosedpage",methods=["POST"])
 def diagnosedpage():
     return render_template("diagnosedpage.html")
@@ -230,12 +176,18 @@ def diagnosedpage():
 @app.route("/diagnosed",methods=["POST"])
 def diagnosed():
     name = request.form.get("name")
-    Date = request.form.get("date")
+    Date = request.form.get("date") #應該有12碼 xxxx_xxxx_xx
     Place = request.form.get("place")
     phone = session["phone"]
-        
-    if( len(str(name))!=0 and len(str(Date))!=0 and len(str(Place))!=0) :
-        ##這是insert to sql 的方法 每一個問號對應一個標籤 
+    
+    #輸入資料合法檢查
+    check_name = 0
+    ACCOUNT_SELECT_QUERY = "SELECT Name FROM Account WHERE ID = '" + phone + "'"
+    accounts = client.sql(ACCOUNT_SELECT_QUERY)
+    for row in accounts :
+        if row[0] == name :
+            check_name = 1
+    if( check_name==1 and len(str(name))!=0 and len(str(Date))==12 and len(str(Place))!=0) :
         r = random.randint(0, 100000000)
         FOOTPRINT_INSERT_QUERY = "INSERT INTO Footprint(Name, ID, Date , Place, key) VALUES ("
         FOOTPRINT_INSERT_QUERY += "\'" + name + "\', "
@@ -243,27 +195,17 @@ def diagnosed():
         FOOTPRINT_INSERT_QUERY += "\'" + Date + "\', "
         FOOTPRINT_INSERT_QUERY += "\'" + Place + "\', "
         FOOTPRINT_INSERT_QUERY += "\'" + str(r) + "\')"
-        #FOOTPRINT_INSERT_QUERY = '''INSERT INTO Footprint(
-        #    Name, ID, Password, Date , Place,key    
-        #) VALUES (?, ?, ?, ?,?,?)'''
-        ##將資料存進一個DATA 再將整個DATA insert
-        #NEW_DATA = [
-        #    [name,phone,"",Date,Place,random.randint(0, 100000000)]  
-        #]
-        #for row in NEW_DATA:
+
         client.sql(FOOTPRINT_INSERT_QUERY)
         return render_template("memberpage.html", msg="輸入成功!")
     else :
-        return render_template("memberpage.html", msg="您未輸入資料!")
+        return render_template("memberpage.html", msg="您未輸入資料 或 資料不符合要求!")
         
-
 
 #/error?msg=錯誤訊息
 @app.route("/error")
 def error():
     msg = request.args.get("msg","發生錯誤")
     return render_template("errorpage.html", msg=msg) #傳入錯誤msg
-
-
 
 app.run() #這要放最後面
